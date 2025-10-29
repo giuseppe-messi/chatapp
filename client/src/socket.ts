f244b807a78f36188c9a5831083fb8dd3d711e4a
giuseppe-messi <giuseppe91messina@gmail.com>
2025-10-29 12:35:09 +0000

    prod setup for both client and server, env vars etc

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -2,7 +2,9 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
+  process.env.NODE_ENV === "production"
+    ? import.meta.env.API_URL
+    : "http://localhost:3000";
 
 // export const socket = io(URL, {
 //   autoConnect: false
