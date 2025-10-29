1901ed2491efa9c890ffc34de56bedf76057dcc1
giuseppe-messi <giuseppe91messina@gmail.com>
2025-10-29 14:48:40 +0000

    cookie prod config setup

--- client/src/baseApi.ts
+++ client/src/baseApi.ts
@@ -3,5 +3,6 @@ import axios from "axios";
 const isProd = import.meta.env.PROD;
 
 export const api = axios.create({
-  baseURL: isProd ? import.meta.env.VITE_API_URL : "/api"
+  baseURL: isProd ? import.meta.env.VITE_API_URL : "/api",
+  withCredentials: true
 });
