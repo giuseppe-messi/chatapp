046368935ad7d6219906da607a13742c7ecde6f1
Joe Messina <giuseppe91messina@gmail.com>
2025-11-30 15:09:59 +0000

    alert about slow service on load

--- client/src/ProtectedRoute.tsx
+++ client/src/ProtectedRoute.tsx
@@ -8,7 +8,21 @@ export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const { user, isLoadingUser } = useAuth();
   const location = useLocation();
 
-  if (isLoadingUser) return <LoadingSpinner size="lg" color="white" />;
+  if (isLoadingUser)
+    return (
+      <div>
+        <div className="bg-[#FFEB3B] max-w-150 mx-auto mt-8 mb-10 p-4 text-sm rounded-md">
+          <p className="mb-4 font-semibold">Heads up</p>
+          <p>
+            The app runs on free-tier hosting (Render & Netlify). It may take a
+            few seconds to wake up after inactivity, so if it seems stuck, try
+            refreshing the page a couple of times, everything works normally
+            once active.
+          </p>
+        </div>
+        <LoadingSpinner size="lg" color="white" />
+      </div>
+    );
 
   if (!user)
     return (
